5e20e2350166f229531c6ebc3a365f7716565ff8
TroyMena <troymena@gmail.com>
2023-12-20 22:53:44 -0500

    heroku test

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
-
+const db = require('./db/db.json')
+// console.log(db)
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -19,11 +25,38 @@ app.get('/notes', (req, res) => {
 });
 
 
+app.get('/api/notes', (req, res) => {
+  // console.log('This is the /api/notes')
+  // console.log(db)
+  res.json(db)
+});
 
+app.post('/api/notes', (req, res) => {
+  // console.log('This is the /api/notes')
+  // console.log(db)
+  console.log("Note saved")
+  console.log(req.body)
 
+  let notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+  
+  };
+  notesData.push(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+  res.json(notesData);
+  Router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+});
 
-
-
+app.delete("/notes/:id", (req, res) => {
+    let notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    notesData = notesData.filter((note) => note.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+    res.json(notesData);
+  });
 
 
 
